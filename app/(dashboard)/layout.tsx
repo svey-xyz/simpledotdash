71240d2307db83fabdd2939a44eb51ea84507421
svey <x@svey.xyz>
2023-10-29 05:43:31 +0000

    Styling

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -35,10 +35,14 @@ export default async function RootLayout({
 	let documentClasses = `${theme ? theme as string : ''} ${inter.className}`
 
 	return (
-		<html lang="en" className={documentClasses}>
+		<html lang="en">
 			<Head />
-			<body className='relative min-h-screen'>
+			<body className='dark relative min-h-screen'>
 				<Header />
+				<div className='fixed top-0 w-full select-none pointer-events-none h-24 z-10 bg-gradient-to-b from-primary-bg to-transparent
+					after:absolute after:inset-0 after:bg-gradient-to-b after:from-primary-bg after:to-transparent'></div>
+				<div className='fixed bottom-0 w-full select-none pointer-events-none h-24 z-10 bg-gradient-to-t from-primary-bg to-transparent
+					after:absolute after:inset-0 after:bg-gradient-to-t after:from-primary-bg after:to-transparent'></div>
 				<main className='mb-24 min-h-full'>
 					{children}
 				</main>
