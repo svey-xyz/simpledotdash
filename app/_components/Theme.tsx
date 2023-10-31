87b7fb38a44dc66d9ed0ee4e38fff9911551c179
svey <x@svey.xyz>
2023-10-31 01:45:04 +0000

    React hooks for theme switching

--- app/_components/Theme.tsx
+++ app/_components/Theme.tsx
@@ -1,47 +1,75 @@
 "use client";
 import { refThemeButton } from "@/app/_components/ThemeButton";
-import React from "react";
+import React, { ReactEventHandler, useEffect } from "react";
 
-let height = window.innerHeight;
+let height: number;
 
 export default function Theme() {
-	
-	setSize();
-	window.addEventListener('resize', setSize);
+	const [darkMode, setDarkMode] = React.useState<boolean>()
+	const buttonLoaded = React.useRef<boolean>(false);
 
-	getDefaultTheme()
+	useEffect(() => {
+		initTheme();
+	}, [global.document]);
+
+	useEffect(() => {
+		initButton();
+	}, [refThemeButton]);
+
+	useEffect(() => {
+		switchTheme()
+	}, [darkMode])
+
+	function initButton() {
+		if (!buttonLoaded.current) {
+			refThemeButton.current?.addEventListener("click", function () {
+				setDarkMode(refThemeButton.current!.checked)
+			})
+			buttonLoaded.current = true;
+		}
+	}
+
+	function initTheme() {
+		const defaultTheme = getDefaultTheme()
+		setDarkMode(defaultTheme === 'dark')
+		switchTheme()
+
+		height = global.window.innerHeight;
+		setSize();
+		global.window.addEventListener('resize', setSize);
+	}
+
+	function getDefaultTheme() {
+		if (!global.window) return;
+		const themeTest = global.window.matchMedia("(prefers-color-scheme: light)");
+		const systemTheme = themeTest.matches ? 'light' : 'dark';
+
+		const userPreference = localStorage.getItem('preferredTheme');
+		const defaultTheme = userPreference ? userPreference : systemTheme;
+
+		return defaultTheme;
+	}
+
+	function switchTheme() {
+		const themeString = darkMode ? 'dark' : 'light'
+		localStorage.setItem('preferredTheme', themeString);
+		localStorage.setItem('theme', themeString);
+		global.document.documentElement.classList[darkMode ? 'add' : 'remove']('dark');
+
+		// if (refThemeButton.current) refThemeButton.current.checked = theme === 'dark'
+	}
+
+	function setSize() {
+		const isMobile = (/Mobi|Android/i.test(navigator.userAgent)) ? true : false;
+
+		if (!isMobile) height = window.innerHeight;
+		const vh = height * 0.01;
+
+		global.document.documentElement.style.setProperty('--vh', `${vh}px`);
+	}
 
 	return (
 		<></>
 	)
 }
 
-function setSize() {
-	const isMobile = (/Mobi|Android/i.test(navigator.userAgent)) ? true : false;
-
-	if (!isMobile) height = window.innerHeight;
-	const vh = height * 0.01;
-
-	global.document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-export function getDefaultTheme() {
-	const themeTest = global.matchMedia("(prefers-color-scheme: light)");
-	const systemTheme = themeTest.matches ? 'light' : 'dark';
-
-	const userPreference = localStorage.getItem('preferredTheme');
-	const defaultTheme = userPreference ? userPreference : systemTheme;
-	global.document.documentElement.classList[defaultTheme === 'dark' ? 'add' : 'remove']('dark');
-
-	return defaultTheme;
-}
-
-export function switchTheme() {
-	const themeState = refThemeButton.current?.checked ? 'dark' : 'light';
-	localStorage.setItem('preferredTheme', themeState);
-	localStorage.setItem('theme', themeState);
-	global.document.documentElement.classList[themeState === 'dark' ? 'add' : 'remove']('dark');
-	console.log(refThemeButton.current?.checked)
-
-	// if (refThemeButton.current) refThemeButton.current.checked = theme === 'dark'
-}
--- app/_components/ThemeButton.tsx
+++ app/_components/ThemeButton.tsx
@@ -2,27 +2,18 @@
 
 export let refThemeButton: RefObject<HTMLInputElement>
 
-import { getDefaultTheme, switchTheme } from "@/app/_components/Theme";
-// import { getDefaultTheme, switchTheme } from "@/app/_components/Theme";
+
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 
 import React, { RefObject, useRef } from "react";
 
 export function ThemeButton() {
+	
 	refThemeButton = useRef<HTMLInputElement>(null)
-
-	const themeButtonClick = () => {
-		switchTheme();
-	}
-	function initTheme(): boolean | undefined {
-		const checked = getDefaultTheme() === 'dark';
-		return checked;
-	}
 
 	return (
 		<label className="group cursor-pointer relative flex items-center justify-center leading-xs w-icon h-icon" >
-			<input id='themeSwitcher' aria-label="Theme Switcher" type="checkbox" ref={refThemeButton} onClick={themeButtonClick}
-				defaultChecked={initTheme()}
+			<input id='themeSwitcher' aria-label="Theme Switcher" type="checkbox" ref={refThemeButton}
 				className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none"/>
 			<SunIcon className="text-primary-text relative w-[80%] block peer-checked:!hidden duration-100 group-hover:scale-[1.3]" />
 			<MoonIcon className="text-primary-text relative w-[80%] !hidden peer-checked:!block duration-100 group-hover:scale-[1.2]" />
