b2ca3fd2f21432957a5afcb14825e111619ef65a
svey <x@svey.xyz>
2023-10-31 19:23:07 +0000

    Better theme switching

--- app/(dashboard)/head.tsx
+++ app/(dashboard)/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
 
 	return (
 		<>
-			{/* <title>{settings.title}</title> */}
+			<title>Dashboard</title>
 			<meta content="width=device-width, initial-scale=1"
 				name="viewport" />
 			<link rel="icon" href="./favicon.ico" />
