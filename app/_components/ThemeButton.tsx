96008c2687baa4215614ade5603edb021f8a1c58
svey <x@svey.xyz>
2023-10-31 17:59:03 +0000

    Next-themes setup

--- app/_components/ThemeButton.tsx
+++ app/_components/ThemeButton.tsx
@@ -1,19 +1,23 @@
 "use client";
 
-export let refThemeButton: RefObject<HTMLInputElement>
-
-
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
-
-import React, { RefObject, useRef } from "react";
+import React, { useEffect, useState } from "react";
+import { useTheme } from 'next-themes'
 
 export function ThemeButton() {
-	
-	refThemeButton = useRef<HTMLInputElement>(null)
+	const [mounted, setMounted] = useState(false)
+	const { theme, setTheme } = useTheme()
+
+	useEffect(() => {
+		setMounted(true)
+	}, [])
+
+	if (!mounted) return null;
 
 	return (
 		<label className="group cursor-pointer relative flex items-center justify-center leading-xs w-icon h-icon" >
-			<input id='themeSwitcher' aria-label="Theme Switcher" type="checkbox" ref={refThemeButton}
+			<input id='themeSwitcher' aria-label="Theme Switcher" type="checkbox" onClick={
+				(e) => { setTheme(e.currentTarget.checked ? 'dark' : 'light') } }
 				className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none"/>
 			<SunIcon className="text-primary-text relative w-[80%] block peer-checked:!hidden duration-100 group-hover:scale-[1.3]" />
 			<MoonIcon className="text-primary-text relative w-[80%] !hidden peer-checked:!block duration-100 group-hover:scale-[1.2]" />
