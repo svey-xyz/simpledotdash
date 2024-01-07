7d905bc1b2ed33a3aa2c941af47b79fbecd31f1d
svey <x@svey.xyz>
2024-01-07 23:23:58 +0000

    Prisma setup 💾

--- lib/data.fetch.ts
+++ lib/data.fetch.ts
@@ -2,6 +2,24 @@ import Ajv from "ajv/dist/jtd"
 import data from "@data/config.json"
 import { AnySchema, AnyValidateFunction } from "ajv/dist/core"
 import { config, schema } from "@/lib/data.schema"
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+	const users = await prisma.user.findMany()
+	console.log(users)
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async (e) => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
 
 const ajv = new Ajv({ allErrors: true }) // options can be passed, e.g. {allErrors: true}
 const validator = ajv.compile(schema as AnySchema)
