bb597640ac8ab041a35fffc05ce2be33599366df
svey <x@svey.xyz>
2024-01-10 22:59:36 +0000

    Remove .env & update .gitignore 🤡

--- components/Signin.tsx
+++ components/Signin.tsx
@@ -1,13 +1,28 @@
 "use client";
 
-import { signIn } from 'next-auth/react';
+import { signIn, useSession, signOut } from 'next-auth/react';
 import React from 'react';
 
 const Signin = ({}:{}) => {
+	const session = useSession()
+	const status = session.status
+	const user = session.data?.user
+
+	const StateBasedButton = session.status == "authenticated" ?
+		(<button onClick={async () => await signOut()}>
+			Sign out
+		</button>) :
+		(<button onClick={async () => await signIn("github")}>
+			Sign in
+		</button>)
 	return (
-		<button onClick={async() => await signIn("github")}>
-			Log in
-		</button>
+		<>
+			{StateBasedButton}
+			{(session.status == "authenticated") &&
+				<p>Logged-in as: {user?.name}</p>
+			}
+		</>
+		
 	);
 };
 
