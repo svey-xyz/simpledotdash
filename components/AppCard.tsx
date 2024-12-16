fd5baadc311927add8268b99b8b06eb80184c5e6
svey-xyz <x@svey.xyz>
2024-12-16 16:32:13 -0500

    App card links

--- components/AppCard.tsx
+++ components/AppCard.tsx
@@ -9,14 +9,22 @@ export const AppCard = ({ app }: {app:App}) => {
 
 
 	return (
-		<div className="group relative flex flex-row border border-transparent-accent rounded-lg py-4 px-6 mt-4 overflow-hidden
-			before:absolute before:inset-0 before:backdrop-blur-xl before:backdrop-saturate-150 before:bg-[rgba(17, 25, 40, 0.75)]">
+		<a
+			className="group relative flex flex-row border border-transparent-accent rounded-lg py-4 px-6 mt-4 overflow-hidden
+			before:absolute before:inset-0 before:backdrop-blur-xl before:backdrop-saturate-150 before:bg-[rgba(17, 25, 40, 0.75)]"
+			href={app.url}
+			target="_blank"
+		>
 			{/* {(Icon && 
 				<div className="text-fg opacity-70 w-8 mr-4 group-hover:scale-[1.12] duration-100 transition-transform group-hover:opacity-100">
 					<Icon />
 				</div>
 			)} */}
-			<h4 className='relative'>{app.title}</h4>
-		</div>
+			<div className="relative">
+				<h4 className='font-bold'>{app.title}</h4>
+				<span className="font-thin">{app.displayURL}</span>
+			</div>
+			
+		</a>
 	)
 }
